4b0f002e519abf5ec5a304045cc8470cffc4fa6e
SergAlKoba <Serg.Al.Koba@gmail.com>
2023-04-14 14:07:33 +0300

    new tem textury

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
-document.getElementById('apply-theme').addEventListener('click', function () {
-  const selectedTheme = document.getElementById('theme-selector').value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { theme: selectedTheme });
-  });
+// document.getElementById('apply-theme').addEventListener('click', function () {
+//   const selectedTheme = document.getElementById('theme-selector').value;
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, { theme: selectedTheme });
+//   });
+// });
+console.log(0)
+
+const menuBtn = document.querySelector('.menu');
+const element = document.querySelector('.stretch.mx-2.flex.flex-row.gap-3');
+console.log(0)
+menuBtn.addEventListener('click', function() {
+  
+  element.classList.add('active');
 });
+
+
