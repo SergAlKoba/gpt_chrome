59f4c3a69cf4e51ccb3f13de8c777198dee7e4cc
Valdemarione <nobodywantz@gmail.com>
2023-04-17 15:24:22 +0300

    Working on contentScript

--- contentScript.js
+++ contentScript.js
@@ -29,7 +29,7 @@ function applyTheme(theme) {
       break;
     default:
       themeHref = 'global_styles.css';
-      
+
       break;
   }
 
@@ -98,23 +98,101 @@ document.body.innerHTML += `
             <a class="discover_more" href="javascript:void(0)">Discover more</a>
         </div>
 `
-document.body.innerHTML += `
-<header class="header_global">
-        <div class="logo">
-            <h2>Got <span>Good.</span></h2>
-        </div>
-        <div class="account">
-            <div class="user">
-                <span>Artificial Intelligence chat room</span>\
-            </div>
-            <div class="settings">
-                <a class="favorites" href="javascript:void(0)"><img src="${chrome.runtime.getURL('./assets/images/favorites.svg')}" alt=""></a>
-                <a class="share" href="javascript:void(0)"><img src="${chrome.runtime.getURL('./assets/images/share.svg')}" alt=""></a>
-                <a class="menu" href="javascript:void(0)"><img src="${chrome.runtime.getURL('assets/images/menu.svg')}" alt=""></a>
-            </div>
-        </div>
-    </header>
-`
+
+let header = document.createElement("header");
+header.className = 'header_global';
+
+let logo = document.createElement("div");
+logo.className = 'logo';
+
+let logoH2 = document.createElement("h2");
+logoH2.innerText = "Got ";
+
+let logoH2Span = document.createElement("span");
+logoH2Span.innerText = "Good.";
+
+let account = document.createElement("div");
+account.className = "account";
+
+let accountUser = document.createElement("div");
+accountUser.className = "user";
+
+let accountUserSpan = document.createElement("span");
+accountUserSpan.innerText = "Artificial Intelligence chat room";
+
+let accountSettings = document.createElement("div");
+accountSettings.className = "settings";
+
+let settingsFavoritesImg = document.createElement("img");
+settingsFavoritesImg.src = chrome.runtime.getURL('./assets/images/favorites.svg');
+settingsFavoritesImg.setAttribute("alt", "");
+
+let settingsShareImg = document.createElement("img");
+settingsShareImg.src = chrome.runtime.getURL('./assets/images/share.svg');
+settingsShareImg.setAttribute("alt", "");
+
+let settingsMenuImg = document.createElement("img");
+settingsMenuImg.src = chrome.runtime.getURL('assets/images/menu.svg');
+settingsMenuImg.setAttribute("alt", "");
+
+let settingsFavoritesLink = document.createElement("a");
+settingsFavoritesLink.className = 'favorites';
+
+let settingsShareLink = document.createElement("a");
+settingsShareLink.className = 'share';
+
+let settingsMenuLink = document.createElement("a");
+settingsMenuLink.className = 'menu';
+
+logoH2.appendChild(logoH2Span);
+logo.appendChild(logoH2);
+header.appendChild(logo);
+accountUser.appendChild(accountUserSpan);
+account.appendChild(accountUser);
+settingsFavoritesLink.appendChild(settingsFavoritesImg);
+settingsShareLink.appendChild(settingsShareImg);
+settingsMenuLink.appendChild(settingsMenuImg);
+accountSettings.appendChild(settingsFavoritesLink);
+accountSettings.appendChild(settingsShareLink);
+accountSettings.appendChild(settingsMenuLink);
+account.appendChild(accountSettings);
+header.appendChild(account);
+document.body.appendChild(header);
+
+// document.body.innerHTML += `
+// <header class="header_global">
+//         <div class="logo">
+//             <h2>Got <span>Good.</span></h2>
+//         </div>
+//         <div class="account">
+//             <div class="user">
+//                 <span>Artificial Intelligence chat room</span>\
+//             </div>
+//             <div class="settings">
+//                 <a class="favorites" href="javascript:void(0)"><img src="${chrome.runtime.getURL('./assets/images/favorites.svg')}" alt=""></a>
+//                 <a class="share" href="javascript:void(0)"><img src="${chrome.runtime.getURL('./assets/images/share.svg')}" alt=""></a>
+//                 <a class="menu" href="javascript:void(0)"><img src="${chrome.runtime.getURL('assets/images/menu.svg')}" alt=""></a>
+//             </div>
+//         </div>
+//     </header>
+// `
+
+
+let themeSettingsContent = document.createElement("div");
+themeSettingsContent.className = "theme_settings_content";
+themeSettingsContent.style = "display: none";
+
+let settingsContent = document.createElement("div");
+settingsContent.className = "settings_content";
+
+let settingsContentForm = document.createElement("form");
+settingsContentForm.setAttribute("action", "");
+
+let settingContentFormButton = document.createElement("button");
+
+let settingContentFormButtonImg = document.createElement("img");
+settingContentFormButtonImg.src = chrome.runtime.getURL('assets/images/search.svg');
+settingContentFormButtonImg.setAttribute("alt", "");
 
 document.body.innerHTML += `
 <div class="theme_settings_content" style="display: none;">
