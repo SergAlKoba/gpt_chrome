d9b9bd22016c2dcab52e936addfe2299395ecf39
George <42513185+RympeR@users.noreply.github.com>
2023-04-26 18:33:32 +0300

    api update

--- contentScripts/requests.js
+++ contentScripts/requests.js
@@ -19,10 +19,10 @@ function getPromptsByCategory(categoryIds, name) {
         redirect: 'follow'
     };
     let result_url = API_URL + '/api/shop/search/?';
-    if (name){
+    if (name) {
         result_url += `name=${name}&`;
     }
-    if (categoryIds){
+    if (categoryIds) {
         categoryIds.forEach(id => {
             result_url += `categories=${id}&`;
         });
@@ -33,100 +33,163 @@ function getPromptsByCategory(categoryIds, name) {
         .catch(error => console.log('error', error));
 }
 
-function getFavorites(){
+function getFavorites() {
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", `token ${TOKEN}`);
-    
+    myHeaders.append("Authorization", `token ${localStorage.getItem('token')}`);
+
     var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
     };
-    
+
     return fetch(API_URL + "/api/chat/get-favorites/", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
 
-function getBookmarks(){
+function getBookmarks() {
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", `token ${TOKEN}`);
-    
+    myHeaders.append("Authorization", `token ${localStorage.getItem('token')}`);
+
     var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
     };
-    
+
     return fetch(API_URL + "/api/chat/get-bookmarks/", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
 
-function login(email, password){
-    var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: {
-        "email": email,
-        "password": password
-    },
-    redirect: 'follow'
-    };
-
-    response =  fetch(API_URL + "/api/user/login/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-    console.log(response);
-    localStorage.setItem('token', response.auth_token);
-    return response;
-}
-
-function register(email, username, password){
-    var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: {
-        "username": username,
-        "email": email,
-        "password": password
-    },
-    redirect: 'follow'
-    };
-
-    response =  fetch(API_URL + "/api/user/register/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-    console.log(response);
-    localStorage.setItem('token', response.auth_token);
-    return response;
-}
-
-function setPromptText(style=none, text=none, tone=none, result_amount=none){
+function createBookmark(output) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "token 6ba254d8e8e18eeb1809a960bb267438aa6c5ab5");
+    myHeaders.append("Authorization", `token ${localStorage.getItem('token')}`);
 
     var raw = JSON.stringify({
-    "style": style,
-    "tone": tone,
-    "result_amount": result_amount,
-    "text": text
+        "output": output
     });
 
     var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(API_URL + "api/chat/create-bookmark/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+function createFavourite(prompt) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", `token ${localStorage.getItem('token')}`);
+
+    var raw = JSON.stringify({
+        "prompt": prompt
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(API_URL + "api/chat/create-favourite/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+function logout() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `token ${localStorage.getItem('token')}`);
+
+
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    return fetch(API_URL + "/api/user/logout/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+function login(email, password) {
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify({
+            "email": email,
+            "password": password
+        }),
+        redirect: 'follow'
+    };
+
+    response = fetch(API_URL + "/api/user/login/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    console.log(response);
+    localStorage.setItem('token', response.auth_token);
+    return response;
+}
+
+function register(email, username, password) {
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify({
+            "username": username,
+            "email": email,
+            "password": password
+        }),
+        redirect: 'follow'
+    };
+
+    response = fetch(API_URL + "/api/user/register/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    console.log(response);
+    localStorage.setItem('token', response.auth_token);
+    return response;
+}
+
+function setPromptText(style = none, text = none, tone = none, result_amount = none) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", `token ${localStorage.getItem('token')}`);
+
+    var raw = JSON.stringify({
+        "style": style,
+        "tone": tone,
+        "result_amount": result_amount,
+        "text": text
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
     };
 
     fetch(API_URL + "/api/shop/get-result-prompt/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
