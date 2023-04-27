2da0e0346ecaa0f8263f96ea43f26c2403a88a7b
Valdemar <nobodywantz@gmail.com>
2023-04-27 22:35:18 +0300

    fix

--- contentScripts/favorites.js
+++ contentScripts/favorites.js
@@ -1,28 +1,28 @@
-// const svgFileUrl = chrome.runtime.getURL('assets/images/favorites.svg');
+const svgFileUrl = chrome.runtime.getURL('assets/images/favorites.svg');
 
-// fetch(svgFileUrl)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         return response.text();
-//     })
-//     .then(svgContent => {
-//         const button = document.createElement('button');
-//         button.textContent = 'button_wait';
-//         button.addEventListener('click', () => {
+fetch(svgFileUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(svgContent => {
+        const button = document.createElement('button');
+        button.textContent = 'button_wait';
+        button.addEventListener('click', () => {
             
-//         });
-//         button.innerHTML = svgContent;
+        });
+        button.innerHTML = svgContent;
         
-//         const div = document.createElement('div');
-//         div.className = 'my-div';
-//         div.style.border = '1px solid black';
-//         div.appendChild(button);
+        const div = document.createElement('div');
+        div.className = 'my-div';
+        div.style.border = '1px solid black';
+        div.appendChild(button);
         
-//         const container = document.body;
-//         container.appendChild(div);
-//     })
-//     .catch(error => {
-//         console.error('Error loading SVG file:', error);
-//     });
+        const container = document.body;
+        container.querySelector("").appendChild(div);
+    })
+    .catch(error => {
+        console.error('Error loading SVG file:', error);
+    });
