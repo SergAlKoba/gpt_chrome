13b9bfd7aa09bd7f3c3ac7a68ce5e3d80558a4b5
Valdemar <nobodywantz@gmail.com>
2023-05-10 18:09:58 +0300

    Fix Modal Input

--- contentScripts/modalInput.js
+++ contentScripts/modalInput.js
@@ -1,111 +1,133 @@
-document.addEventListener('readystatechange', event => {
-  
-  const container = $('textarea').parentElement;
+const items = [
+  { text: 'Make this more consistent', command: 'Make this more consistent' },
+  { text: 'Tell me more about this', command: 'Tell me more about this' },
+  { text: 'Expand details', command: 'Expand details' },
+  { text: 'Give me better suggestions', command: 'Give me better suggestions' },
+  { text: 'Wrap this up', command: 'Wrap this up' },
+];
 
-  container.appendChild(prompster);
-  const textArea = $('textarea')
-  const selectorPromster = $(".prompster");
-  const selectorUlPromster = selectorPromster.querySelector("ul");
-  const search = document.getElementById('site-search');
-
-  search.addEventListener('blur', () => {
-    selectorPromster.classList.remove('active');
-    selectorUlPromster.classList.remove('active');
+function createElementModal(tag, attributes, children) {
+  const elem = document.createElement(tag);
+  Object.keys(attributes).forEach(key => {
+    elem.setAttribute(key, attributes[key]);
   });
-  textArea.addEventListener('keydown', function () {
-    console.log(textArea.value);
-    if (textArea.value == '/') {
-      selectorPromster.classList.add('active');
-      selectorUlPromster.classList.add('active');
-      // search.focus();
-    } else {
-      console.log('not /', textArea.value);
-    }
+  children.forEach(child => {
+    elem.appendChild(child);
   });
-});
+  return elem;
+}
 
 function sendModalInput(selected_prompt) {
   console.log(selected_prompt);
-  let send_button = $('form > div > div.flex.flex-col.w-full.py-2.flex-grow.rounded-md> button');
-
-  $("textarea").value = selected_prompt;
+  let send_button = document.querySelector('form > div > div.flex.flex-col.w-full.py-2.flex-grow.rounded-md> button');
+  document.querySelector("textarea").value = selected_prompt;
   send_button.removeAttribute('disabled');
   send_button.click();
 }
 
-const items = [
-  'Make this more consistent',
-  'Tell me more about this',
-  'Expand details',
-  'Give me better suggestions',
-  'Wrap this up',
-];
+function createUlFromItems(items) {
+  const liItems = [];
+  items.forEach(item => {
+    const textNode = document.createTextNode(item.text);
+    const li = createElementModal('li', { class: 'prompster-item visible', value: item.command }, [textNode]);
 
-let elements = [{
-    'text': 'Make this more consistent',
-    'command': 'Make this more consistent'
-  },
-  {
-    'text': 'Tell me more about this',
-    'command': 'Tell me more about this'
-  },
-  {
-    'text': 'Expand details',
-    'command': 'Expand details'
-  },
-  {
-    'text': 'Give me better suggestions',
-    'command': 'Give me better suggestions'
-  },
-  {
-    'text': 'Wrap this up',
-    'command': 'Wrap this up'
-  },
-];
-createUlFromItems(items);
-
-let ul = document.createElement('ul');
-for (let i = 0; i < elements.length; i++) {
-  let li = document.createElement('li');
-  li.classList.add('prompster-item');
-  li.innerHTML = elements[i].text;
-  ul.appendChild(li);
-  li.addEventListener('click', () => {
-    sendModalInput(elements[i].command)
-    selectorPromster.classList.remove('active');
-    selectorUlPromster.classList.remove('active');
+    liItems.push(li);
   });
+
+  return createElementModal('ul', { id: 'prompsterList' }, liItems);
 }
-
-let prompster = document.createElement('div');
-prompster.classList.add('prompster');
-prompster.appendChild(ul);
-
-let prompster_input = document.createElement('input');
-prompster_input.setAttribute('type', 'search');
-prompster_input.setAttribute('id', 'site-search');
-prompster_input.setAttribute('name', 'prompster');
-prompster.appendChild(prompster_input);
-prompster_input.oninput = (e) => {
-  filterPrompterItems(e.target.value);
-};
 
 function filterPrompterItems(searchText) {
   const normalizeText = (text) => {
     return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
   };
-  Array.from($All(".prompster-item")).forEach(item => {
+  Array.from(document.querySelectorAll(".prompster-item")).forEach(item => {
     const itemText = normalizeText(item.textContent);
 
     const searchRegExp = new RegExp(normalizeText(searchText), 'g');
     if (!itemText.match(searchRegExp)) {
       item.style.display = "none";
+      item.classList.remove("visible");
     } else {
       item.style.display = "list-item";
+      
+      item.classList.add("visible");
     }
 
   });
 
 }
+function createPrompster() {
 
-// container.insertAdjacentHTML('afterbegin', prompster);
+  const prompsterInput = createElementModal('input', {
+    type: 'search',
+    id: 'site-search',
+    name: 'prompster',
+  }, []);
+
+  prompsterInput.oninput = (e) => {
+    filterPrompterItems(e.target.value);
+  };
+
+  const ul = createUlFromItems(items);
+  const prompster = createElementModal('div', { id: 'prompster', class: 'prompster' }, [ul, prompsterInput]);
+  ul.querySelectorAll('.prompster-item').forEach((li) => {
+    li.onclick = () => {
+      sendModalInput(li.innerText);
+      prompster.classList.remove('active');
+      ul.classList.remove('active');
+    };
+  })
+
+  return prompster;
+}
+
+function addPrompster() {
+  const container = document.querySelector('textarea').parentElement;
+  container.appendChild(createPrompster());
+  const textArea = document.querySelector('textarea');
+  const selectorPromster = document.querySelector("#prompster");
+  const selectorUlPromster = selectorPromster.querySelector("ul");
+  const search = document.getElementById('site-search');
+
+  search.addEventListener('keydown', (e) => {
+
+    if (e.key == 'Enter') {
+      e.preventDefault();
+      document.querySelector('.prompster-item.visible').click();
+
+    }
+  })
+
+  search.addEventListener('blur', () => {
+    setTimeout(() => {
+      selectorPromster.classList.remove('active');
+      selectorUlPromster.classList.remove('active');
+    }, 100);
+  });
+
+  textArea.addEventListener('keydown', function (e) {
+    if (e.key === '/') {
+      e.preventDefault();
+      selectorPromster.classList.add('active');
+      selectorUlPromster.classList.add('active');
+      search.focus();
+    } else {
+      console.log('not /', textArea.value);
+    }
+  });
+
+}
+
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.type === 'childList') {
+      const idPrompster = document.getElementById('prompster');
+      if (idPrompster == null || idPrompster == undefined) {
+        addPrompster();
+      }
+    }
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
