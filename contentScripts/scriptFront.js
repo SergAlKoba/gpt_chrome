683cc3c66f3238abaa37f146828418fd003c9999
SergAlKoba <Serg.Al.Koba@gmail.com>
2023-05-31 01:11:51 +0300

    fix bug and new bar

---
+++ contentScripts/scriptFront.js
@@ -0,0 +1,26 @@
+var $win = $(window);
+var $doc = $(document);
+
+;(function($) {
+
+    $doc.ready(function(){
+
+        $('.item_nav').on('click', function(){
+            // $('.promt_item').removeClass('active');
+            $(this).parent('.promt_item').toggleClass('active');
+        });
+       
+    });
+
+})(jQuery)
+
+;(function($) {
+
+ 
+  
+})(jQuery)
+
+
+
+
+
